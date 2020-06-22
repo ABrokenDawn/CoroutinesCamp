54ac5b68bfa8c23985a6e860227c79d7effb5293
Kai Zhu <rengwuxian@gmail.com>
2020-06-22 16:17:16 +0800

    初始化

--- app/src/main/java/com/hencoder/coroutinescamp/PracticeActivity1.kt
+++ app/src/main/java/com/hencoder/coroutinescamp/PracticeActivity1.kt
@@ -1,24 +1,27 @@
 package com.hencoder.coroutinescamp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class PracticeActivity1 : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_practice1)
 
     val data = getData()
     val processedData = processData(data)
+    // 练习内容：用协程让上面 ↑ 这两行放在后台执行，然后把代码截图贴到腾讯课堂的作业里
     textView.text = processedData
   }
 
+  // 耗时函数 1
   private fun getData(): String {
     // 假设这个函数比较耗时，需要放在后台
     return "hen_coder"
   }
 
+  // 耗时函数 2
   private fun processData(data: String): String {
     // 假设这个函数也比较耗时，需要放在后台
     return data.split("_") // 把 "hen_coder" 拆成 ["hen", "coder"]
